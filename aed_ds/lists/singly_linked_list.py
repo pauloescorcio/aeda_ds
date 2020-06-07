0e0b6a4fd827bd058d2931d92863b7ae17372e6e
Paulo Escórcio <pauloescorcio@Escorcio.local>
2020-06-07 01:43:04 +0100

    SinglyLinkedList Update

--- aed_ds/lists/singly_linked_list.py
+++ aed_ds/lists/singly_linked_list.py
@@ -1,141 +1,208 @@
-from .tad_list import List
 from .nodes import SingleListNode
+from ..exceptions import EmptyListException, InvalidPositionException
 from .singly_linked_list_iterator import SinglyLinkedListIterator
-from ..exceptions import EmptyListException, InvalidPositionException, NoSuchElementException
 
+class SinglyLinkedList ():
 
-class SinglyLinkedList(List):
-    
-    def __init__(self):
+    def __init__ (self):
         self.head = None
         self.tail = None
         self.num_elements = 0
 
+    # Returns true iff the list contains no elements.
+    
     def is_empty(self):
         return self.num_elements == 0
+
+    # Returns the number of elements in the list.
 
     def size(self):
         return self.num_elements
 
-    def get_first(self): 
-        if not self.head:
-            raise EmptyListException
+    # Returns the first element of the list.
+
+    # Throws EmptyListException.
+
+    def get_first(self):
+        if self.is_empty():
+            raise EmptyListException()
         return self.head.get_element()
 
-    def get_last(self): 
-        if not self.head:
-            raise EmptyListException
+    # Returns the last element of the list.
+
+    # Throws EmptyListException.
+
+    def get_last(self):
+        if self.is_empty():
+            raise EmptyListException()
+            pass
         return self.tail.get_element()
 
-    def get(self, position): 
-        if not self.head:
-            raise EmptyListException()
-        cur_node = self.head
-        idx = 0
-        while cur_node:
-            if idx == position:
-                return cur_node.get_element()
-            cur_node = cur_node.get_next()
-            idx += 1
 
-    def find(self, element): 
-        cur_node = self.head
-        idx = 0
-        while cur_node:
-            if element == cur_node.get_element():
-                return idx
-            cur_node = cur_node.get_next()
-            idx += 1
-        return -1            
+    # Returns the element at the specified position in the list.
+
+    # Range of valid positions: 0, ..., size()-1.
+
+    def get(self, position):
+        if self.is_empty():
+            raise EmptyListException()
+        pointer = self.head
+        index = 0
+        while pointer != None:
+            if position == index:
+                return pointer.get_element()
+            pointer = pointer.get_next()
+            index = index + 1
+
+
+    # Returns the position in the list of the
+
+    # first occurrence of the specified element,
+
+    # or -1 if the specified element does not
+
+    # occur in the list.
+
+    def find(self, element):
+        pointer = self.head
+        index = 0
+        while pointer != None:
+            if pointer.get_element() == element:
+                return index
+            pointer = pointer.get_next()
+            index = index + 1
+        return -1
+
+
+    # Inserts the specified element at the first position in the list.
+
 
     def insert_first(self, element):
-        new_node = SingleListNode(element, self.head)
-        if not self.head:
+        new_node = SingleListNode (element, self.head)
+        if self.is_empty():
             self.tail = new_node
-        self.head = new_node
-        self.num_elements += 1
-    
-    def insert_last(self, element):
-        new_node = SingleListNode(element, None)
-        if not self.head: 
+            self.head = self.tail
+        else:
             self.head = new_node
-        else:    
-            self.tail.set_next(new_node)
-        self.tail = new_node
-        self.num_elements += 1
+        self.num_elements = self.num_elements + 1
 
-    def insert(self, element, position): 
+
+    # Inserts the specified element at the last position in the list.
+
+    def insert_last(self, element):
+        new_node = SingleListNode (element, None)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+        self.num_elements = self.num_elements + 1
+     
+
+
+    # Inserts the specified element at the specified position in the list.
+
+    # Range of valid positions: 0, ..., size().
+
+    # If the specified position is 0, insert corresponds to insertFirst.
+
+    # If the specified position is size(), insert corresponds to insertLast.
+
+    # Throws InvalidPositionException.
+
+    def insert(self, element, position):
         if position < 0 or position > self.size():
             raise InvalidPositionException()
         elif position == 0:
             return self.insert_first(element)
         elif position == self.size():
             return self.insert_last(element)
-        prev_node = self.head
-        cur_node = self.head
-        idx = 0
-        while prev_node:
-            if position == idx: 
-                new_node = SingleListNode(element, cur_node)
-                prev_node.set_next(new_node)
-                self.num_elements += 1
+        previous_pointer = self.head
+        pointer = self.head
+        index = 0
+        while previous_pointer != None:
+            if index == position:
+                new_node = SingleListNode (element, pointer)
+                previous_pointer.set_next(new_node)
+                self.num_elements = self.num_elements + 1
                 break
-            prev_node = cur_node
-            cur_node = cur_node.get_next()
-            idx += 1 
+            previous_pointer = pointer
+            pointer = pointer.get_next()
+            index = index + 1
+
+    # Removes and returns the element at the first position in the list.
+
+    # Throws EmptyListException.
 
     def remove_first(self):
-        if not self.head:
+        if self.is_empty():
             raise EmptyListException()
         elif self.head == self.tail:
-            result = self.head.get_element()
+            result  = self.head.get_element()
             self.make_empty()
             return result
         else:
             old_head = self.head
             self.head = self.head.get_next()
-            self.num_elements -= 1
+            self.num_elements = self.num_elements - 1
             return old_head.get_element()
 
-    def remove_last(self): 
-        if not self.head:
-            raise EmptyListException()
-        elif self.size() == 1:
-            return self.remove_first()
-        cur_node = self.head
-        while cur_node.get_next().get_next() is not None:
-            cur_node = cur_node.get_next()
-        old_node = self.tail
-        self.tail = cur_node
-        cur_node.set_next(None)
-        self.num_elements -= 1
-        return old_node.get_element()
+    # Removes and returns the element at the last position in the list.
 
-    def remove(self, position): 
+    # Throws EmptyListException.
+
+    def remove_last(self):
+        if self.is_empty ():
+            raise EmptyListException()
+        elif self.head == self.tail:
+            result = self.head.get_element()
+            self.make_empty()
+            return result
+        pointer = self.head
+        while pointer.get_next().get_next() != None:
+            pointer = pointer.get_next()
+        old_pointer = pointer.get_next().get_element()
+        pointer.set_next (None)
+        self.tail = pointer
+        self.num_elements = self.num_elements - 1
+        return old_pointer
+
+    
+    # Removes and returns the element at the specified position in the list.
+
+    # Range of valid positions: 0, ..., size()-1.
+
+    # Throws InvalidPositionException.
+
+    def remove(self, position):
         if position < 0 or position > self.size() - 1:
             raise InvalidPositionException()
         elif position == 0:
             return self.remove_first()
         elif position == self.size() - 1:
             return self.remove_last()
-        cur_node = self.head
-        idx = 0
-        while cur_node:
-            if idx == position:
-                prev_node.set_next(cur_node.get_next())
-                old_node = cur_node
-                cur_node.set_next(None)
-                self.num_elements -= 1
-                return old_node.get_element()
-            prev_node = cur_node
-            cur_node = cur_node.get_next()
-            idx += 1
+        pointer = self.head
+        old_pointer = self.head
+        index = 0
+        while pointer != None:
+            if position == index:
+                old_pointer.set_next(pointer.get_next())
+                result = pointer.get_element()
+                break
+            old_pointer = pointer
+            pointer = pointer.get_next()
+            index+=1
+        return result
+
+    # Removes all elements from the list.
 
     def make_empty(self):
         self.head = None
         self.tail = None
-        self.num_elements = 0    
+        self.num_elements = 0
+ 
+    # Returns an iterator of the elements in the list (in proper sequence).
 
     def iterator(self):
         return SinglyLinkedListIterator(self)
-
--- tasks.py
+++ tasks.py
@@ -11,38 +11,6 @@ def cov(c):
     c.run("coverage html")
 
 @task
-def setupstream(c, username="amgs"):
-    remotes = c.run("git remote", hide=True).stdout
-    if "upstream" not in remotes:
-        origin_url = c.run("git remote get-url origin", hide=True).stdout.split("\n")[0]
-        project_name = re.search(f"\/\/.+\/.+\/(.+?)\.git$", origin_url).group(1)
-        c.run(f"git remote add upstream https://github.com/{username}/{project_name}.git")
-
-@task(setupstream)
-def sync(c):
-    c.run("git fetch upstream")
-    c.run("git merge upstream/master")
-
-@task
-def pr(c):
-    c.run("hub pr list --format='[%I] %cI %uI %t [%H] | %U%n'")
-
-@task
-def clean(c):
-    c.run("if exist %CD%\dist rmdir /S /Q dist")
-    c.run("if exist %CD%\\build rmdir /S /Q build")
-    c.run("if exist %CD%\\aed_ds.egg-info rmdir /S /Q aed_ds.egg-info")
-
-@task(clean)
-def build(c):
-    c.run("python setup.py bdist_wheel")
-
-@task(build)
-def install(c):
-    c.run("pip uninstall -y aed-ds")
-    c.run("pip install --find-links=%cd%\dist aed_ds")
-
-@task
 def xinstall(c):
     c.run("pip uninstall -y aed-ds")
     c.run("rm -rf dist/ build/ *.egg-info/")
